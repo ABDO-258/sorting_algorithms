7078ba1dc581b23aa994f513d8b47cdf10fe1471
abdo <abdoraja2005@gmail.com>
2023-09-22 14:21:21 +0100

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort a list in ascending order using intertion
+ *
+ * @list: pointer to a pointer to the head of the list
+ *
+ *
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp, *sorted = NULL;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return;
+
+	while ((*list) != NULL)
+	{
+		print_list(sorted);
+
+		current = (*list);
+
+		(*list) = (*list)->next;
+		if (sorted == NULL || current->n < sorted->n)
+		{
+			current->next = sorted;
+			sorted = current;
+		}
+		else
+		{
+			temp = sorted;
+			while (temp != NULL)
+			{
+				if (temp->next == NULL || current->n < temp->next->n)
+				{
+					current->next = temp->next;
+					temp->next = current;
+					break;
+				}
+				temp = temp->next;
+			}
+		}
+	}
+}
