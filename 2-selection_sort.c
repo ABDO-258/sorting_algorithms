ffd9fec7d15c56ad427031fad7e27ee1fa509a33
abdo <abdoraja2005@gmail.com>
2023-09-22 20:41:02 +0100

    task 2 handle duplicate value

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,7 +21,7 @@ void selection_sort(int *array, size_t size)
 		min = array[i];
 		for (k = i + 1; k < size  ; k++)
 		{
-			if (array[k] < min)
+			if (array[k] <= min)
 			{
 				min = array[k];
 				j = k;
