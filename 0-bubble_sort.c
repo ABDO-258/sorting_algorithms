41ed4f8a9eaf7262257ffc48b11b7c7c622f1056
abdo <abdoraja2005@gmail.com>
2023-09-22 13:00:30 +0100

    task 0 bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort an array of integers in ascending order
+ *
+ * @array: the array to be sorted
+ * @size:size of the array to be sorted
+ *
+ *
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp, swap;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = 0;
+
+		for (j = 0; j < size - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap = 1;
+				print_array(array, size);
+			}
+
+		}
+		if (swap == 0)
+			break;
+	}
+}
